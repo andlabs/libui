dc2967619c74d0e454fb6f5e4655670537814515
Pietro Gagliardi <pietro10@mac.com>
2015-04-18 19:58:57 -0400

    Turned off the Page 1 controls for now. This will be for testing drawing the proper background on Windows parents.

--- test.c
+++ test.c
@@ -316,7 +316,8 @@ int main(int argc, char *argv[])
 	uiStackAppend(stacks[0], uiControl(label), 0);
 
 	tab = uiNewTab();
-	uiTabAddPage(tab, "Page 1", uiControl(stacks[0]));
+//TODO	uiTabAddPage(tab, "Page 1", uiControl(stacks[0]));
+	uiTabAddPage(tab, "Page 1", uiControl(uiNewVerticalStack()));
 
 	page2stack = i;
 	stacks[i] = uiNewVerticalStack();

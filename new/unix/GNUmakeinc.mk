647b7892ef1d600d4526aba265a80c4b332f9b9f
Pietro Gagliardi <pietro10@mac.com>
2015-04-22 15:04:55 -0400

    Added nonworking makefiles...

---
+++ new/unix/GNUmakeinc.mk
@@ -0,0 +1,18 @@
+# 22 april 2015
+
+osCFILES = \
+	main.c
+
+osHFILES = \
+	unix/uipriv_unix.h
+
+osCFLAGS = \
+	-fvisibility=hidden \
+	`pkg-config --cflags gtk+-3.0`
+
+osLDFLAGS = \
+	-fvisibility=hidden \
+	`pkg-config --libs gtk+-3.0`
+
+osLIBSUFFIX = .so
+osEXESUFFIX =

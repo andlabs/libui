b9f855eaecacf51c025fe87cc4c439d6ba829047
Pietro Gagliardi <pietro10@mac.com>
2015-04-23 19:23:14 -0400

    Expanded the menu test. GTK+ is really starting to get on my nerves.

--- new/test/menus.c
+++ new/test/menus.c
@@ -10,9 +10,22 @@ uiMenuItem *undoItem;
 uiMenuItem *checkItem;
 uiMenuItem *accelItem;
 uiMenuItem *prefsItem;
+uiMenu *testMenu;
+uiMenuItem *enabledItem;
+uiMenuItem *enableThisItem;
+uiMenuItem *forceCheckedItem;
+uiMenuItem *forceUncheckedItem;
 uiMenu *helpMenu;
 uiMenuItem *helpItem;
 uiMenuItem *aboutItem;
+
+static void enableItemTest(uiMenuItem *item, uiWindow *w, void *data)
+{
+	if (uiMenuItemChecked(enabledItem))
+		uiMenuItemEnable(enableThisItem);
+	else
+		uiMenuItemDisable(enableThisItem);
+}
 
 void initMenus(void)
 {
@@ -27,6 +40,14 @@ void initMenus(void)
 	checkItem = uiMenuAppendCheckItem(editMenu, "Check Me\tTest");
 	accelItem = uiMenuAppendItem(editMenu, "A&ccele&&rator T_es__t");
 	prefsItem = uiMenuAppendPreferencesItem(editMenu);
+
+	testMenu = uiNewMenu("Test");
+	enabledItem = uiMenuAppendCheckItem(testMenu, "Enable Below Item");
+	uiMenuItemSetChecked(enabledItem, 1);
+	uiMenuItemOnClicked(enabledItem, enableItemTest, NULL);
+	enableThisItem = uiMenuAppendItem(testMenu, "This Will Be Enabled");
+	forceCheckedItem = uiMenuAppendItem(testMenu, "Force Above Checked");
+	forceUncheckedItem = uiMenuAppendItem(testMenu, "Force Above Unchecked");
 
 	helpMenu = uiNewMenu("Help");
 	helpItem = uiMenuAppendItem(helpMenu, "Help");

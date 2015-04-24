e4e2c1232ff6980a54f3f3bf5a3395c21079f5ef
Pietro Gagliardi <pietro10@mac.com>
2015-04-23 22:32:58 -0400

    Started readding the Windows backend. Still not too sure about uiOSContainer...

---
+++ new/windows/alloc.c
@@ -0,0 +1,49 @@
+// 4 december 2014
+#include "uipriv_windows.h"
+
+// wrappers for allocator of choice
+// panics on memory exhausted, undefined on heap corruption or other unreliably-detected malady (see http://stackoverflow.com/questions/28761680/is-there-a-windows-api-memory-allocator-deallocator-i-can-use-that-will-just-giv)
+// new memory is set to zero
+// passing NULL to tableRealloc() acts like tableAlloc()
+// passing NULL to tableFree() is a no-op
+
+void *uiAlloc(size_t size, const char *type)
+{
+	void *out;
+
+	out = malloc(size);
+	if (out == NULL) {
+		fprintf(stderr, "memory exhausted in uiAlloc() allocating %s\n", type);
+		abort();
+	}
+	ZeroMemory(out, size);
+	if (options.debugLogAllocations)
+		fprintf(stderr, "%p alloc %s\n", out, type);
+	return out;
+}
+
+void *uiRealloc(void *p, size_t size, const char *type)
+{
+	void *out;
+
+	if (p == NULL)
+		return uiAlloc(size, type);
+	out = realloc(p, size);
+	if (out == NULL) {
+		fprintf(stderr, "memory exhausted in uiRealloc() reallocating %s\n", type);
+		abort();
+	}
+	// TODO zero the extra memory
+	if (options.debugLogAllocations)
+		fprintf(stderr, "%p realloc %p\n", p, out);
+	return out;
+}
+
+void uiFree(void *p)
+{
+	if (p == NULL)
+		return;
+	free(p);
+	if (options.debugLogAllocations)
+		fprintf(stderr, "%p free\n", p);
+}

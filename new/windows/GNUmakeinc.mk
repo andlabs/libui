3ea84f9bf5c793cb93a97fbaab1547633cb0cbbe
Pietro Gagliardi <pietro10@mac.com>
2015-04-24 13:59:49 -0400

    More Windows work. This will just be a flat import for now. I really want to figure something out about uiOSContainers...

---
+++ new/windows/GNUmakeinc.mk
@@ -0,0 +1,35 @@
+# 22 april 2015
+
+osCFILES = \
+	windows/alloc.c \
+	windows/init.c \
+	windows/main.c \
+	windows/oscontainer.c \
+	windows/text.c \
+	windows/util.c \
+	windows/window.c
+
+osHFILES = \
+	windows/uipriv_windows.h
+
+# thanks ebassi in irc.gimp.net/#gtk+
+osCFLAGS = \
+	-D_UI_EXTERN='__declspec(dllexport) extern'
+
+osLDFLAGS = \
+	-luser32 -lkernel32 -lgdi32 -luxtheme -lmsimg32 -lcomdlg32 -lole32 -loleaut32 -loleacc -luuid
+
+osLIBSUFFIX = .dll
+osEXESUFFIX = .exe
+
+ifeq ($(ARCH),64)
+	CC = x86_64-w64-mingw32-gcc
+	RC = x86_64-w64-mingw32-windres
+	osCFLAGS += -m64
+	osLDFLAGS += -m64
+else
+	CC = i686-w64-mingw32-gcc
+	RC = i686-w64-mingw32-windres
+	osCFLAGS += -m32
+	osLDFLAGS += -m32
+endif
---
+++ new/windows/uipriv_windows.h
@@ -0,0 +1,39 @@
+// 24 april 2015
+#define UNICODE
+#define _UNICODE
+#define STRICT
+#define STRICT_TYPED_ITEMIDS
+#define CINTERFACE
+#define COBJMACROS
+// see https://github.com/golang/go/issues/9916#issuecomment-74812211
+#define INITGUID
+// get Windows version right; right now Windows XP
+#define WINVER 0x0501
+#define _WIN32_WINNT 0x0501
+#define _WIN32_WINDOWS 0x0501		/* according to Microsoft's winperf.h */
+#define _WIN32_IE 0x0600			/* according to Microsoft's sdkddkver.h */
+#define NTDDI_VERSION 0x05010000	/* according to Microsoft's sdkddkver.h */
+#include <windows.h>
+#include <commctrl.h>
+#include <stdint.h>
+#include <uxtheme.h>
+#include <string.h>
+#include <wchar.h>
+#include <windowsx.h>
+#include <vsstyle.h>
+#include <vssym32.h>
+#include <stdarg.h>
+#include <oleacc.h>
+#include <stdio.h>
+#include "../ui.h"
+#include "../uipriv.h"
+#include "../ui_windows.h"
+
+// ui internal window messages
+enum {
+	// redirected WM_COMMAND and WM_NOTIFY
+	msgCOMMAND = WM_APP + 0x40,		// start offset just to be safe
+	msgNOTIFY,
+	msgUpdateChild,		// fake because Windows seems to SWP_NOSIZE MoveWindow()s and SetWindowPos()s that don't change the window size (even if SWP_NOSIZE isn't specified)
+	msgCanDestroyNow,
+};

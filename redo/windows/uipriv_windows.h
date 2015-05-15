883db84eaf6faa688284b63094e5c72ff9706c40
Pietro Gagliardi <pietro10@mac.com>
2015-05-15 16:19:21 -0400

    Some uipriv_windows.h cleanup.

--- redo/windows/uipriv_windows.h
+++ redo/windows/uipriv_windows.h
@@ -34,15 +34,9 @@ enum {
 	// redirected WM_COMMAND and WM_NOTIFY
 	msgCOMMAND = WM_APP + 0x40,		// start offset just to be safe
 	msgNOTIFY,
-	msgUpdateChild,		// fake because Windows seems to SWP_NOSIZE MoveWindow()s and SetWindowPos()s that don't change the window size (even if SWP_NOSIZE isn't specified)
 	msgHasTabStops,
 	msgConsoleEndSession,
 };
-
-// debug.c
-extern HRESULT logLastError(const char *);
-extern HRESULT logHRESULT(const char *, HRESULT);
-extern HRESULT logMemoryExhausted(const char *);
 
 // init.c
 extern HINSTANCE hInstance;
@@ -57,39 +51,14 @@ extern DWORD getStyle(HWND);
 extern void setStyle(HWND, DWORD);
 extern DWORD getExStyle(HWND);
 extern void setExStyle(HWND, DWORD);
-extern void moveWindow(HWND, intmax_t, intmax_t, intmax_t, intmax_t);
-extern void moveAndReorderWindow(HWND, HWND, intmax_t, intmax_t, intmax_t, intmax_t);
 
 // text.c
 extern WCHAR *toUTF16(const char *);
 extern char *toUTF8(const WCHAR *);
 extern WCHAR *windowText(HWND);
 
-// comctl32.c
-extern BOOL (*WINAPI fv_SetWindowSubclass)(HWND, SUBCLASSPROC, UINT_PTR, DWORD_PTR);
-extern BOOL (*WINAPI fv_RemoveWindowSubclass)(HWND, SUBCLASSPROC, UINT_PTR);
-extern LRESULT (*WINAPI fv_DefSubclassProc)(HWND, UINT, WPARAM, LPARAM);
-extern const char *initCommonControls(void);
 
-// window.c
-extern ATOM registerWindowClass(HICON, HCURSOR);
-extern void unregisterWindowClass(void);
 
-// container.c
-extern HWND initialParent;
-extern const char *initContainer(HICON, HCURSOR);
-extern void uninitContainer(void);
-
-// menu.c
-extern HMENU makeMenubar(void);
-extern const uiMenuItem *menuIDToItem(UINT_PTR);
-extern void runMenuEvent(WORD, uiWindow *);
-extern void freeMenubar(HMENU);
-extern void uninitMenus(void);
-
-// alloc.c
-extern int initAlloc(void);
-extern void uninitAlloc(void);
 
 // tab.c
 extern void tabEnterTabNavigation(HWND);
@@ -106,8 +75,33 @@ extern void moveWindow(HWND, intmax_t, intmax_t, intmax_t, intmax_t);
 extern void moveAndReorderWindow(HWND, HWND, intmax_t, intmax_t, intmax_t, intmax_t);
 
 // utilwindow.c
+extern HWND utilWindow;
 extern const char *initUtilWindow(HICON, HCURSOR);
 extern void uninitUtilWindow(void);
 
 // parent.c
 extern BOOL handleParentMessages(HWND, UINT, WPARAM, LPARAM, LRESULT *);
+
+// debug.c
+extern HRESULT logLastError(const char *);
+extern HRESULT logHRESULT(const char *, HRESULT);
+extern HRESULT logMemoryExhausted(const char *);
+
+// window.c
+extern ATOM registerWindowClass(HICON, HCURSOR);
+extern void unregisterWindowClass(void);
+
+// container.c
+extern const char *initContainer(HICON, HCURSOR);
+extern void uninitContainer(void);
+
+// menu.c
+extern HMENU makeMenubar(void);
+extern const uiMenuItem *menuIDToItem(UINT_PTR);
+extern void runMenuEvent(WORD, uiWindow *);
+extern void freeMenubar(HMENU);
+extern void uninitMenus(void);
+
+// alloc.c
+extern int initAlloc(void);
+extern void uninitAlloc(void);

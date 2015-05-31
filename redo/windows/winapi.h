c45aeb31a0362ae902bd939d8ecc4109e7693149
Pietro Gagliardi <pietro10@mac.com>
2015-05-31 18:45:11 -0400

    Dropped XP support from windows/winapi.h. The next few commits will move to Vista-specific features and APIs.

--- redo/windows/winapi.h
+++ redo/windows/winapi.h
@@ -7,12 +7,12 @@
 #define COBJMACROS
 // see https://github.com/golang/go/issues/9916#issuecomment-74812211
 #define INITGUID
-// get Windows version right; right now Windows XP
-#define WINVER 0x0501
-#define _WIN32_WINNT 0x0501
-#define _WIN32_WINDOWS 0x0501		/* according to Microsoft's winperf.h */
-#define _WIN32_IE 0x0600			/* according to Microsoft's sdkddkver.h */
-#define NTDDI_VERSION 0x05010000	/* according to Microsoft's sdkddkver.h */
+// get Windows version right; right now Windows Vista
+#define WINVER 0x0600				/* according to Microsoft's winnls.h */
+#define _WIN32_WINNT 0x0600		/* according to Microsoft's sdkddkver.h */
+#define _WIN32_WINDOWS 0x0600		/* according to Microsoft's pdh.h */
+#define _WIN32_IE 0x0700			/* according to Microsoft's sdkddkver.h */
+#define NTDDI_VERSION 0x06000000	/* according to Microsoft's sdkddkver.h */
 #include <windows.h>
 #include <commctrl.h>
 #include <stdint.h>

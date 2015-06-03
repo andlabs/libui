0b7dc948228c58e9aba9753b51fe32da713b02f4
Pietro Gagliardi <pietro10@mac.com>
2015-06-03 15:19:37 -0400

    Fixed up the layout of test/pge5.c. The change necessary to make it work will come next.

--- redo/test/page5.c
+++ redo/test/page5.c
@@ -60,14 +60,13 @@ uiBox *makePage5(void)
 
 	page5 = newVerticalBox();
 
-	// TODO label should not be stretchy - figure out how to autosize it on text change
 #define D(n, f) \
 	hbox = newHorizontalBox(); \
 	button = uiNewButton(n); \
 	label = uiNewLabel(""); \
 	uiButtonOnClicked(button, f, label); \
 	uiBoxAppend(hbox, uiControl(button), 0); \
-	uiBoxAppend(hbox, uiControl(label), 1); \
+	uiBoxAppend(hbox, uiControl(label), 0); \
 	uiBoxAppend(page5, uiControl(hbox), 0);
 
 	D("Open File", openFile);

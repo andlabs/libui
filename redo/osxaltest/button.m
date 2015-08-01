32905f598999e3ce22270f5530291d93fba84d87
Pietro Gagliardi <pietro10@mac.com>
2015-07-31 23:06:29 -0400

    More Auto Layout test work.

---
+++ redo/osxaltest/button.m
@@ -0,0 +1,37 @@
+// 31 july 2015
+#import "osxaltest.h"
+
+@interface tButton : NSObject<tControl> {
+	NSButton *b;
+}
+@end
+
+@implementation tButton
+
+- (id)initWithText:(NSString *)text
+{
+	self = [super init];
+	if (self) {
+		self->b = [[NSButton alloc] initWithFrame:NSZeroRect];
+		[self->b setTitle:text];
+		[self->b setButtonType:NSMomentaryPushInButton];
+		[self->b setBordered:YES];
+		[self->b setBezelStyle:NSRoundedBezelStyle];
+		[self->b setFont:[NSFont systemFontOfSize:[NSFont systemFontSizeForControlSize:NSRegularControlSize]]];
+	}
+	return self;
+}
+
+- (void)tAddToView:(NSView *)v
+{
+	[v addSubview:self->b];
+}
+
+- (uintmax_t)tAddToAutoLayoutDictionary:(NSMutableDictionary *)views keyNumber:(uintmax_t)n
+{
+	[views setObject:self->b forKey:tAutoLayoutKey(n)];
+	n++;
+	return n;
+}
+
+@end
--- redo/osxaltest/osxaltest.h
+++ redo/osxaltest/osxaltest.h
@@ -1,2 +1,11 @@
 // 31 july 2015
 #import <Cocoa/Cocoa.h>
+#import <stdint.h>
+
+@protocol tControl
+@required
+- (void)tAddToView:(NSView *)v;
+- (uintmax_t)tAddToAutoLayoutDictionary:(NSMutableDictionary *)views keyNumber:(uintmax_t)n;
+@end
+
+extern NSString *tAutoLayoutKey(uintmax_t);

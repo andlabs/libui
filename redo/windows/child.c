99c37b96977abed6f07c51b4dfb7fe315ddce534
Pietro Gagliardi <pietro10@mac.com>
2015-08-31 10:55:33 -0400

    Wrote up the boilerplate needed for tab pages.

--- redo/windows/child.c
+++ redo/windows/child.c
@@ -7,11 +7,13 @@ struct child {
 	uiControl *c;
 	HWND hwnd;
 
-	HWND parent;
+	// This is a helper for uiTab pages.
+	// For visual accuracy of tab page backgrounds, margins are also handled here, applied to the child only (rather than applied to the whole tab page).
+	HWND tabpage;
+	int margined;
 
 	// This flag is for users of these functions.
 	// For uiBox, this is "spaced".
-	// For uiTab, this is "margined". (uiGroup and uiWindow have to maintain their margined state themselves, since the margined state is independent of whether there is a child for those two.)
 	int flag;
 };
 
@@ -28,8 +30,18 @@ struct child *newChild(uiControl *child, uiControl *parent, HWND parentHWND)
 
 	uiControlSetParent(c->c, parent);
 	uiWindowsEnsureSetParent(c->hwnd, parentHWND);
-	c->parent = parentHWND;
 
+	return c;
+}
+
+struct child *newChildWithTabPage(uiControl *child, uiControl *parent, HWND parentHWND)
+{
+	struct child *c;
+	HWND tabpage;
+
+	tabpage = newTabPage();
+	c = newChild(child, parent, tabpage);
+	uiWindowsEnsureSetParent(tabpage, parentHWND);
 	return c;
 }
 
@@ -37,6 +49,8 @@ void childRemove(struct child *c)
 {
 	uiWindowsEnsureSetParent(c->hwnd, utilwin);
 	uiControlSetParent(c->c, NULL);
+	if (c->tabpage != NULL)
+		uiWindowsEnsureDestroyWindow(c->tabpage);
 	uiFree(c);
 }
 
@@ -57,15 +71,34 @@ HWND childHWND(struct child *c)
 void childMinimumSize(struct child *c, uiWindowsSizing *d, intmax_t *width, intmax_t *height)
 {
 	uiWindowsControl *wc;
+	intmax_t left, top, right, bottom
 
 	wc = uiWindowsControl(c->c);
 	(*(wc->MinimumSize))(wc, d, width, height);
+	if (c->tabpage != NULL && c->margined) {
+		tabPageMargins(c->tabpage, &left, &top, &right, &bottom);
+		*width += left + right;
+		*height += top + bottom;
+	}
 }
 
 void childRelayout(struct child *c, intmax_t x, intmax_t y, intmax_t width, intmax_t height)
 {
 	uiWindowsControl *wc;
+	intmax_t left, top, right, bottom;
 
+	if (c->tabpage != NULL) {
+		uiWindowsEnsureMoveWindow(c->tabpage, x, y, width, height);
+		x = 0;		// and make relative to the client rect of the tab page
+		y = 0;
+		if (c->margined) {
+			tabPageMargins(c->tabpage, &left, &top, &right, &bottom);
+			x += left;
+			y += top;
+			width -= left + right;
+			height -= top + bottom;
+		}
+	}
 	wc = uiWindowsControl(c->c);
 	(*(wc->Relayout))(wc, x, y, width, height);
 }
@@ -73,6 +106,22 @@ void childRelayout(struct child *c, intmax_t x, intmax_t y, intmax_t width, intm
 void childUpdateState(struct child *c)
 {
 	controlUpdateState(c->c);
+}
+
+HWND childTabPage(struct child *c)
+{
+	return c->tabpage;
+}
+
+int childMargined(struct child *c)
+{
+	return c->margined;
+}
+
+void childSetMargined(struct child *c)
+{
+	c->margined = margined;
+	uiControlQueueResize(c->c);
 }
 
 int childFlag(struct child *c)
---
+++ redo/windows/tabpage.c
@@ -0,0 +1,62 @@
+// 30 may 2015
+#include "uipriv_windows.h"
+
+// This just defines the implementation of the tab page HWND itself.
+// The actual work of hosting a tab page's control is in child.c.
+
+// from http://msdn.microsoft.com/en-us/library/windows/desktop/bb226818%28v=vs.85%29.aspx
+#define tabMargin 7
+
+static void tabPageMargins(HWND hwnd, intmax_t *left, intmax_t *right, intmax_t *right, intmax_t *bottom)
+{
+	uiWindowsSizing *d;
+
+	d = uiWindowsNewSizing(hwnd);
+	*left = uiWindowsDlgUnitsToX(tabMargin, d->BaseX);
+	*top = uiWindowsDlgUnitsToY(tabMargin, d->BaseY);
+	*right = *left;
+	*bottom = *top;
+	uiWindowsFreeSizing(d);
+}
+
+// dummy dialog procedure; see below for details
+// let's handle parent messages here to avoid needing to subclass
+static INT_PTR CALLBACK dlgproc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	LRESULT lResult;
+
+	if (handleParentMessages(hwnd, uMsg, wParam, lParam, &lResult) != FALSE) {
+		SetWindowLongPtrW(hwnd, DWLP_MSGRESULT, (LONG_PTR) lResult);
+		return TRUE;
+	}
+	// unthemed dialogs don't respond to WM_PRINTCLIENT
+	// fortunately they don't have any special painting
+	if (uMsg == WM_PRINTCLIENT) {
+		// don't worry about the return value; hopefully DefWindowProcW() caught it (if not the dialog procedure itself)
+		// we COULD paint the dialog background brush ourselves but meh, it works
+		SendMessageW(hwnd, WM_ERASEBKGND, wParam, lParam);
+		// and pretend we did nothing just so the themed dialog can still paint its content
+		return FALSE;
+	}
+	if (uMsg == WM_INITDIALOG)
+		return TRUE;
+	return FALSE;
+}
+
+HWND newTabPage(void)
+{
+	HWND hwnd;
+	HRESULT hr;
+
+	// unfortunately this needs to be a proper dialog for EnableThemeDialogTexture() to work; CreateWindowExW() won't suffice
+	hwnd = CreateDialogW(hInstance, MAKEINTRESOURCE(rcTabPageDialog),
+		utilWindow, dlgproc);
+	if (t->hwnd == NULL)
+		logLastError("error creating tab page in newTabPage()");
+
+	hr = EnableThemeDialogTexture(t->hwnd, ETDT_ENABLE | ETDT_USETABTEXTURE | ETDT_ENABLETAB);
+	if (hr != S_OK)
+		logHRESULT("error setting tab page background in newTabPage()", hr);
+
+	return hwnd;
+}

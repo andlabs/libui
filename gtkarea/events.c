3f15f5d60a1870d5576cc1f19afb119a7c67e25c
Pietro Gagliardi <pietro10@mac.com>
2015-09-13 10:43:26 -0400

    Finished migrating events.c.

--- gtkarea/events.c
+++ gtkarea/events.c
@@ -74,88 +74,96 @@ Non-typewriter keys can be handled safely using constants provided by the respec
 Because GTK+ keysyms may or may not obey Num Lock, we also handle the 0-9 and . keys on the numeric keypad with scancodes (they match too).
 */
 
-/*
-TODO
+// use uintptr_t to be safe; the size of the scancode/hardware key code field on each platform is different
+static const struct {
+	uintptr_t scancode;
+	char equiv;
+} scancodeKeys[] = {
+	{ 0x02, '1' },
+	{ 0x03, '2' },
+	{ 0x04, '3' },
+	{ 0x05, '4' },
+	{ 0x06, '5' },
+	{ 0x07, '6' },
+	{ 0x08, '7' },
+	{ 0x09, '8' },
+	{ 0x0A, '9' },
+	{ 0x0B, '0' },
+	{ 0x0C, '-' },
+	{ 0x0D, '=' },
+	{ 0x0E, '\b' },
+	{ 0x0F, '\t' },
+	{ 0x10, 'q' },
+	{ 0x11, 'w' },
+	{ 0x12, 'e' },
+	{ 0x13, 'r' },
+	{ 0x14, 't' },
+	{ 0x15, 'y' },
+	{ 0x16, 'u' },
+	{ 0x17, 'i' },
+	{ 0x18, 'o' },
+	{ 0x19, 'p' },
+	{ 0x1A, '[' },
+	{ 0x1B, ']' },
+	{ 0x1C, '\n' },
+	{ 0x1E, 'a' },
+	{ 0x1F, 's' },
+	{ 0x20, 'd' },
+	{ 0x21, 'f' },
+	{ 0x22, 'g' },
+	{ 0x23, 'h' },
+	{ 0x24, 'j' },
+	{ 0x25, 'k' },
+	{ 0x26, 'l' },
+	{ 0x27, ';' },
+	{ 0x28, '\'' },
+	{ 0x29, '`' },
+	{ 0x2B, '\\' },
+	{ 0x2C, 'z' },
+	{ 0x2D, 'x' },
+	{ 0x2E, 'c' },
+	{ 0x2F, 'v' },
+	{ 0x30, 'b' },
+	{ 0x31, 'n' },
+	{ 0x32, 'm' },
+	{ 0x33, ',' },
+	{ 0x34, '.' },
+	{ 0x35, '/' },
+	{ 0x39, ' ' },
+	{ 0xFFFF, 0 },
+};
 
-// use uintptr to be safe; the size of the scancode/hardware key code field on each platform is different
-var scancodeKeys = map[uintptr]byte{
-	0x02: '1',
-	0x03: '2',
-	0x04: '3',
-	0x05: '4',
-	0x06: '5',
-	0x07: '6',
-	0x08: '7',
-	0x09: '8',
-	0x0A: '9',
-	0x0B: '0',
-	0x0C: '-',
-	0x0D: '=',
-	0x0E: '\b',
-	0x0F: '\t',
-	0x10: 'q',
-	0x11: 'w',
-	0x12: 'e',
-	0x13: 'r',
-	0x14: 't',
-	0x15: 'y',
-	0x16: 'u',
-	0x17: 'i',
-	0x18: 'o',
-	0x19: 'p',
-	0x1A: '[',
-	0x1B: ']',
-	0x1C: '\n',
-	0x1E: 'a',
-	0x1F: 's',
-	0x20: 'd',
-	0x21: 'f',
-	0x22: 'g',
-	0x23: 'h',
-	0x24: 'j',
-	0x25: 'k',
-	0x26: 'l',
-	0x27: ';',
-	0x28: '\'',
-	0x29: '`',
-	0x2B: '\\',
-	0x2C: 'z',
-	0x2D: 'x',
-	0x2E: 'c',
-	0x2F: 'v',
-	0x30: 'b',
-	0x31: 'n',
-	0x32: 'm',
-	0x33: ',',
-	0x34: '.',
-	0x35: '/',
-	0x39: ' ',
+static const struct {
+	uintptr_t scancode;
+	uiExtKey equiv;
+} scancodeExtKeys[] = {
+	{ 0x47, uiExtKeyN7 },
+	{ 0x48, uiExtKeyN8 },
+	{ 0x49, uiExtKeyN9 },
+	{ 0x4B, uiExtKeyN4 },
+	{ 0x4C, uiExtKeyN5 },
+	{ 0x4D, uiExtKeyN6 },
+	{ 0x4F, uiExtKeyN1 },
+	{ 0x50, uiExtKeyN2 },
+	{ 0x51, uiExtKeyN3 },
+	{ 0x52, uiExtKeyN0 },
+	{ 0x53, uiExtKeyNDot },
+	{ 0xFFFF, 0 },
+};
+
+int fromScancode(uintptr_t scancode, uiAreaKeyEvent *ke)
+{
+	int i;
+
+	for (i = 0; scancodeKeys[i].scancode != 0xFFFF; i++)
+		if (scancodeKeys[i].scancode == scancode) {
+			ke->Key = scancodeKeys[i].equiv;
+			return 1;
+		}
+	for (i = 0; scancodeExtKeys[i].scancode != 0xFFFF; i++)
+		if (scancodeExtKeys[i].scancode == scancode) {
+			ke->ExtKey = scancodeExtKeys[i].equiv;
+			return 1;
+		}
+	return 0;
 }
-
-var scancodeExtKeys = map[uintptr]ExtKey{
-	0x47: N7,
-	0x48: N8,
-	0x49: N9,
-	0x4B: N4,
-	0x4C: N5,
-	0x4D: N6,
-	0x4F: N1,
-	0x50: N2,
-	0x51: N3,
-	0x52: N0,
-	0x53: NDot,
-}
-
-func fromScancode(scancode uintptr) (ke KeyEvent, ok bool) {
-	if key, ok := scancodeKeys[scancode]; ok {
-		ke.Key = key
-		return ke, true
-	}
-	if extkey, ok := scancodeExtKeys[scancode]; ok {
-		ke.ExtKey = extkey
-		return ke, true
-	}
-	return ke, false
-}
-
-*/

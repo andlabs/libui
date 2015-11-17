786c0a33d4b2c9786d90303dd78129a107b62096
Pietro Gagliardi <pietro10@mac.com>
2015-11-17 12:09:51 -0500

    More TODOs.

--- haiku/GNUosspecific.mk
+++ haiku/GNUosspecific.mk
@@ -4,6 +4,7 @@ EXESUFFIX =
 LIBSUFFIX = .so
 OSHSUFFIX = .hpp
 
+# TODO dynamically select the gcc4 compiler instead? gotta figure out how to do this on a GCC 2 Hybrid nightly
 gccver = $(shell $(CC) --version | sed 's/-.*//g')
 ifeq ($(gccver),2.95.3)
 $(error Sorry; GCC 4 is necessary to build libui for Haiku)

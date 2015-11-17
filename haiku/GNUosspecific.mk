486ca161d39c394bcae61318481073796b959192
Pietro Gagliardi <pietro10@mac.com>
2015-11-17 11:55:52 -0500

    Removed gcc2 support in Haiku.

--- haiku/GNUosspecific.mk
+++ haiku/GNUosspecific.mk
@@ -3,3 +3,8 @@
 EXESUFFIX =
 LIBSUFFIX = .so
 OSHSUFFIX = .hpp
+
+gccver = $(shell $(CC) --version | sed 's/-.*//g')
+ifeq ($(gccver),2.95.3)
+$(error Sorry; GCC 4 is necessary to build libui for Haiku)
+endif

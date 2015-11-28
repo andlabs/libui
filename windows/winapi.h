99176a1d10b463b647541b62a56ae9aed06255b9
Pietro Gagliardi <pietro10@mac.com>
2015-11-27 20:05:48 -0500

    Disabled compiler warnings for system headers on MSVC. Removed /Wp64; it's a no-op on VS2013 (our minimum supported version).

--- windows/winapi.h
+++ windows/winapi.h
@@ -16,6 +16,10 @@
 #define _WIN32_WINDOWS 0x0600		/* according to Microsoft's pdh.h */
 #define _WIN32_IE 0x0700			/* according to Microsoft's sdkddkver.h */
 #define NTDDI_VERSION 0x06000000	/* according to Microsoft's sdkddkver.h */
+// TODO cl.exe spews garbage warnings for system headers; figure out which flags cause it
+#ifdef _MSC_VER
+#pragma warning(push, 0)
+#endif
 #include <windows.h>
 // Microsoft's resource compiler will segfault if we feed it headers it was not designed to handle
 #ifndef RC_INVOKED
@@ -34,4 +38,7 @@
 #include <math.h>
 #include <d2d1.h>
 #include <float.h>
+#endif
+#ifdef _MSC_VER
+#pragma warning(pop)
 #endif

8adb1a25a27107e853b19e4637e5f5c6cf124dd1
Pietro Gagliardi <pietro10@mac.com>
2015-11-28 12:16:05 -0500

    More TODOs.

--- darwin/map.m
+++ darwin/map.m
@@ -1,6 +1,8 @@
 // 17 august 2015
 #import "uipriv_darwin.h"
 
+// TODO are the NSValues being garbage collected underfoot? open menuless window and check the checkbox
+
 // unfortunately NSMutableDictionary copies its keys, meaning we can't use it for pointers
 // hence, this file
 

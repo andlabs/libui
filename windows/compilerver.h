be6555d885a24cd8a5c0058cb97c11e9df622b55
Pietro Gagliardi <pietro10@mac.com>
2015-12-09 15:31:14 -0500

    More MinGW removal stuff.

--- windows/compilerver.h
+++ windows/compilerver.h
@@ -9,21 +9,8 @@
 #endif
 
 // MinGW
-// Only MinGW-w64 is supported due to supporting APIs introduced after 2001.
-// 4.0.0 is required due to its improved Vista support.
 #ifdef __MINGW32__
-#ifndef __MINGW64_VERSION_MAJOR
-#error At present, only MinGW-w64 (>= 4.0.0) is supported. Other toolchains will be supported in the future (but other variants of MinGW will not).
-#endif
-#if __MINGW64_VERSION_MAJOR > 4
-#define ui_good_mingw
-#elif (__MINGW64_VERSION_MAJOR == 4) && (__MINGW64_VERSION_MINOR > 0)
-#define ui_good_mingw
-#endif
-#ifndef ui_good_mingw
-#error MinGW-w64 version 4.0.0 or newer is required.
-#endif
-#undef ui_good_mingw
+#error At present, MinGW is not supported; see README.md for details.
 #endif
 
 // other compilers can be added here as necessary

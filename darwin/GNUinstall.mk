bdded52f5ede489057c55dcb63e6a2cfa33ac2a2
Florent Clairambault <florent@clairambault.fr>
2016-01-01 23:20:49 +0100

    Mac Os X install rule: Tiny fix.

--- darwin/GNUinstall.mk
+++ darwin/GNUinstall.mk
@@ -5,5 +5,5 @@ endif
 # Incorrect for Mac Os X, this should be easy to fix
 install: $(OUT)
 	cp $(OUT) $(DESTDIR)$(PREFIX)/lib/libui.0.dylib
-	ln -s libui.so.0 $(DESTDIR)$(PREFIX)/lib/libui.so
+	ln -s libui.0.dylib $(DESTDIR)$(PREFIX)/lib/libui.dylib
 	cp ui.h ui_$(OS).h $(DESTDIR)$(PREFIX)/include/

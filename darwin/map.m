f9b940f698069ab3396b8e8a1b269b47f56b09e5
Pietro Gagliardi <pietro10@mac.com>
2016-01-06 20:39:47 -0500

    More TODOs.

--- darwin/map.m
+++ darwin/map.m
@@ -1,7 +1,7 @@
 // 17 august 2015
 #import "uipriv_darwin.h"
 
-// TODO are the NSValues being garbage collected underfoot? open menuless window and check the checkbox
+// TODO are the NSValues (or worse, the NSMapTable) being garbage collected underfoot? open menuless window and check the checkbox; crash the package ui test when closing the main window; etc.
 
 // unfortunately NSMutableDictionary copies its keys, meaning we can't use it for pointers
 // hence, this file

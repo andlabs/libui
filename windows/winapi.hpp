55116199aca1f4b791171cc0c954e63c53a7c886
Pietro Gagliardi <pietro10@mac.com>
2016-04-20 19:51:30 -0400

    Pruned, organized, and formatted winapi.hpp.

--- windows/winapi.hpp
+++ windows/winapi.hpp
@@ -3,35 +3,45 @@
 #define _UNICODE
 #define STRICT
 #define STRICT_TYPED_ITEMIDS
+
 // see https://github.com/golang/go/issues/9916#issuecomment-74812211
 // TODO get rid of this
 #define INITGUID
+
+// TODO see if we can remove this
 #define _USE_MATH_DEFINES
+
 // for the manifest
 #define ISOLATION_AWARE_ENABLED 1
+
 // get Windows version right; right now Windows Vista
-#define WINVER 0x0600				/* according to Microsoft's winnls.h */
-#define _WIN32_WINNT 0x0600		/* according to Microsoft's sdkddkver.h */
-#define _WIN32_WINDOWS 0x0600		/* according to Microsoft's pdh.h */
-#define _WIN32_IE 0x0700			/* according to Microsoft's sdkddkver.h */
-#define NTDDI_VERSION 0x06000000	/* according to Microsoft's sdkddkver.h */
+// unless otherwise stated, all values from Microsoft's sdkddkver.h
+// TODO is all of this necessary? how is NTDDI_VERSION used?
+// TODO plaform update sp2
+#define WINVER			0x0600	/* from Microsoft's winnls.h */
+#define _WIN32_WINNT		0x0600
+#define _WIN32_WINDOWS	0x0600	/* from Microsoft's pdh.h */
+#define _WIN32_IE			0x0700
+#define NTDDI_VERSION		0x06000000
+
 #include <windows.h>
+
 // Microsoft's resource compiler will segfault if we feed it headers it was not designed to handle
 #ifndef RC_INVOKED
 #include <commctrl.h>
+#include <windowsx.h>
+#include <shobjidl.h>
+#include <d2d1.h>
+#include <dwrite.h>
+
 #include <stdint.h>
-#include <uxtheme.h>
 #include <string.h>
 #include <wchar.h>
-#include <windowsx.h>
-#include <vsstyle.h>
-#include <vssym32.h>
 #include <stdarg.h>
-#include <oleacc.h>
 #include <stdio.h>
-#include <shobjidl.h>
 #include <math.h>
-#include <d2d1.h>
 #include <float.h>
-#include <dwrite.h>
+
+#include <vector>
+#include <map>
 #endif

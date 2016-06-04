0bca3e21da57c7e7a13153c849eb92d8a342b369
Pietro Gagliardi <pietro10@mac.com>
2016-06-04 15:52:55 -0400

    Even more old build tools removal. Next will be a one-fell-swoop thing.

--- migrate_build/unix/GNUinstall.mk
+++
@@ -1,8 +0,0 @@
-ifndef PREFIX
-    PREFIX=/usr
-endif
-
-install: $(OUT)
-	cp $(OUT) $(DESTDIR)$(PREFIX)/lib/libui.so.0
-	ln -fs libui.so.0 $(DESTDIR)$(PREFIX)/lib/libui.so
-	cp ui.h ui_$(OS).h $(DESTDIR)$(PREFIX)/include/

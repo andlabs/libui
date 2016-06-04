acbeb86c7ff70a69f159273a63b607802b9599e2
Pietro Gagliardi <pietro10@mac.com>
2016-06-04 18:52:40 -0400

    Pruned out and merged in th erest of the migrate_build files.

--- migrate_build/darwin/GNUfiles.mk
+++
@@ -1,4 +0,0 @@
-
-# on warning about undefined symbols:
-# the gcc flags don't work with Apple's linker
-# fortunately, we don't need any; Apple's linker warns about undefined symbols in -shared builds!

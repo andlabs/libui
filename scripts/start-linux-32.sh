861aa591092b45ff9feaa13f0ff325c442906f7a
Andrea Parodi <andrea@parro.it>
2016-06-20 22:19:07 +0200

    short arts

--- scripts/start-linux-32.sh
+++ scripts/start-linux-32.sh
@@ -1,3 +1,4 @@
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   docker run -i -v "${PWD}:/libui-32" andystanton/gcc-cmake-gl /bin/bash -c "linux32 --32bit i386 /libui-32/scripts/build-32.sh"
 fi
+ls ./artifacts

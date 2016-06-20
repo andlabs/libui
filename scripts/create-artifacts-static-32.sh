d07ae2404c5273369e4499d8f704df2b198b3a23
Andrea Parodi <andrea@parro.it>
2016-06-20 21:58:26 +0200

    use start docker script

--- scripts/create-artifacts-static-32.sh
+++ scripts/create-artifacts-static-32.sh
@@ -1,3 +1,3 @@
 cd out
-tar -cvf ../../artifacts/examples-static-$TRAVIS_OS_NAME-x64-$TRAVIS_TAG.tar.gz controlgallery cpp-multithread histogram
-tar -cvf ../../artifacts/libui-static-$TRAVIS_OS_NAME-x64-$TRAVIS_TAG.tar.gz libui.a
+tar -cvf ../../artifacts/examples-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz controlgallery cpp-multithread histogram
+tar -cvf ../../artifacts/libui-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz libui.a
--- scripts/start-linux-32.sh
+++ scripts/start-linux-32.sh
@@ -1,1 +1,3 @@
-docker run -i -v "${PWD}:/libui-32" toopher/centos-i386:centos6 /bin/bash -c "linux32 --32bit i386 /libui-32/scripts/build-32.sh"
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  docker run -i -v "${PWD}:/libui-32" toopher/centos-i386:centos6 /bin/bash -c "linux32 --32bit i386 /libui-32/scripts/build-32.sh"
+fi

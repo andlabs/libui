36e0c984a84fd0f74edec42d279b3668a80ce6bd
Andrea Parodi <andrea@parro.it>
2016-06-20 23:03:28 +0200

    removedls

--- scripts/start-linux-32.sh
+++ scripts/start-linux-32.sh
@@ -1,4 +1,3 @@
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   docker run -e "TRAVIS_OS_NAME=$TRAVIS_OS_NAME" -e "TRAVIS_TAG=$TRAVIS_TAG" -i -v "${PWD}:/libui-32" leandrosouza/cmake-gcc /bin/bash -c "linux32 --32bit i386 /libui-32/scripts/build-32.sh"
 fi
-ls ./artifacts

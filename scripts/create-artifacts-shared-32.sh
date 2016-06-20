06332c2f28d5c85cb1db134264f7983723d0217c
Andrea Parodi <andrea@parro.it>
2016-06-20 22:38:01 +0200

    pass env

--- scripts/create-artifacts-shared-32.sh
+++ scripts/create-artifacts-shared-32.sh
@@ -1,8 +1,5 @@
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  rm out/libui.so;
-  LIB_FILE=libui.so.0;
-fi
-
+rm out/libui.so;
+LIB_FILE=libui.so.0;
 cd out
 tar -cvf ../../artifacts/examples-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz controlgallery cpp-multithread histogram $LIB_FILE
 tar -cvf ../../artifacts/libui-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz $LIB_FILE

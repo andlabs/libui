87b8142eb9511248f131be68a5497ca312d77044
Andrea Parodi <andrea@parro.it>
2016-06-21 22:35:39 +0200

    readd suo

--- scripts/run-32.sh
+++ scripts/run-32.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd /libui-32;
-apt-get update;
-apt-get install libc6-dev-i386 -y;
-apt-get install libgtk-3-dev:i386 -y;
+sudo apt-get update;
+sudo apt-get install libc6-dev-i386 -y;
+sudo apt-get install libgtk-3-dev:i386 -y;
 ./scripts/build-32.sh

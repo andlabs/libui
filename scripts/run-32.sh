4eeae6c7235478c8c97893f1d370c13bed09c98c
Andrea Parodi <andrea@parro.it>
2016-06-21 22:20:16 +0200

    force 32bit pkg

--- scripts/run-32.sh
+++ scripts/run-32.sh
@@ -3,5 +3,5 @@
 cd /libui-32;
 apt-get update;
 apt-get install libc6-dev-i386 -y;
-apt-get install libgtk-3-dev -y || apt-cache search libgtk3;
+apt-get install libgtk-3-dev:i386 -y;
 ./scripts/build-32.sh

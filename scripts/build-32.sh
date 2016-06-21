bce1f02e56232f1878f2ae6162ac93fc5c8c53db
Andrea Parodi <andrea@parro.it>
2016-06-21 21:34:58 +0200

    changed dir

--- scripts/build-32.sh
+++ scripts/build-32.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+cd /libui-32;
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  echo faked > ../../artifacts/examples-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
-  echo faked > ../../artifacts/libui-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
-  echo faked > ../../artifacts/examples-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
-  echo faked > ../../artifacts/libui-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
+  echo faked > ./artifacts/examples-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
+  echo faked > ./artifacts/libui-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
+  echo faked > ./artifacts/examples-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
+  echo faked > ./artifacts/libui-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
 else
   export BUILD_ARCH=ia32
   cd build

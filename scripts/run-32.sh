a99701bc704aaf54d41cd4a10d58d518890fe471
Andrea Parodi <andrea@parro.it>
2016-06-21 21:56:10 +0200

    try sudo removal

--- scripts/run-32.sh
+++ scripts/run-32.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /libui-32;
-sudo apt-get update;
-sudo apt-get install libc6-dev-i386;
-sudo apt-get install libgtk-3-dev -y || sudo apt-cache search libgtk3;
+apt-get update;
+apt-get install libc6-dev-i386;
+apt-get install libgtk-3-dev -y || apt-cache search libgtk3;
 ./scripts/build-32.sh

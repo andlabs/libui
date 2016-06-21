dd094b1a9d2f422fb4ec2030b2777702a2c079df
Andrea Parodi <andrea@parro.it>
2016-06-21 22:03:47 +0200

    add confirmation

--- scripts/run-32.sh
+++ scripts/run-32.sh
@@ -2,6 +2,6 @@
 
 cd /libui-32;
 apt-get update;
-apt-get install libc6-dev-i386;
+apt-get install libc6-dev-i386 -y;
 apt-get install libgtk-3-dev -y || apt-cache search libgtk3;
 ./scripts/build-32.sh

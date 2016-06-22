48803a20242cd27193fd9ca1983703434b8ffaf7
Andrea Parodi <a.parodi@inet2.it>
2016-06-22 13:51:46 +0200

    include header files in libs

--- scripts/create-artifacts-shared.sh
+++ scripts/create-artifacts-shared.sh
@@ -4,4 +4,4 @@ LIB_FILE=libui.*;
 
 cd out
 tar -cvf ../../artifacts/examples-shared-$TRAVIS_OS_NAME-$BUILD_ARCH-$TRAVIS_TAG.tar.gz controlgallery cpp-multithread histogram $LIB_FILE
-tar -cvf ../../artifacts/libui-shared-$TRAVIS_OS_NAME-$BUILD_ARCH-$TRAVIS_TAG.tar.gz $LIB_FILE
+tar -cvf ../../artifacts/libui-shared-$TRAVIS_OS_NAME-$BUILD_ARCH-$TRAVIS_TAG.tar.gz $LIB_FILE ../../*.h
--- scripts/create-artifacts-static.sh
+++ scripts/create-artifacts-static.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd out
 tar -cvf ../../artifacts/examples-static-$TRAVIS_OS_NAME-$BUILD_ARCH-$TRAVIS_TAG.tar.gz controlgallery cpp-multithread histogram
-tar -cvf ../../artifacts/libui-static-$TRAVIS_OS_NAME-$BUILD_ARCH-$TRAVIS_TAG.tar.gz libui.a
+tar -cvf ../../artifacts/libui-static-$TRAVIS_OS_NAME-$BUILD_ARCH-$TRAVIS_TAG.tar.gz libui.a ../../*.h
 

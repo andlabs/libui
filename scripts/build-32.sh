21118b0b99936de2a637e989921bf7defde8ee01
Andrea Parodi <andrea@parro.it>
2016-06-22 07:22:57 +0200

    temp disable 32bit

--- scripts/build-32.sh
+++ scripts/build-32.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  echo faked > ./artifacts/examples-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
-  echo faked > ./artifacts/libui-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
-  echo faked > ./artifacts/examples-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
-  echo faked > ./artifacts/libui-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
-else
-  export BUILD_ARCH=ia32
-  cd build
-  ../scripts/clean-build-dir.sh
-  ../scripts/configure-static.sh
-  make tester examples
-  ../scripts/create-artifacts-static.sh
-  ../scripts/clean-build-dir.sh
-  ../scripts/configure-shared.sh
-  make tester examples
-  ../scripts/create-artifacts-shared.sh
-fi
+export BUILD_ARCH=ia32
+cd build
+../scripts/clean-build-dir.sh
+../scripts/configure-static.sh
+make tester examples
+../scripts/create-artifacts-static.sh
+../scripts/clean-build-dir.sh
+../scripts/configure-shared.sh
+make tester examples
+../scripts/create-artifacts-shared.sh
 
--- scripts/start-linux-32.sh
+++ scripts/start-linux-32.sh
@@ -2,4 +2,9 @@
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   linux32 --32bit i386 ./scripts/run-32.sh
-fi
+else
+  echo faked > ./artifacts/examples-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
+  echo faked > ./artifacts/libui-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
+  echo faked > ./artifacts/examples-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
+  echo faked > ./artifacts/libui-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
+else

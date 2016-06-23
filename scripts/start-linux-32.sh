5276839a1670cc1e640a89c2ac012480db674b3e
Andrea Parodi <andrea@parro.it>
2016-06-23 16:49:15 +0200

    run 32bit

--- scripts/start-linux-32.sh
+++ scripts/start-linux-32.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-#  linux32 --32bit i386 ./scripts/run-32.sh
-#else
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  linux32 --32bit i386 ./scripts/run-32.sh
+else
   echo faked > ./artifacts/examples-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
   echo faked > ./artifacts/libui-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
   echo faked > ./artifacts/examples-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
   echo faked > ./artifacts/libui-shared-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
-#fi
+fi

d70274d7c04325c08e45cf7cab24883e4ae3f2de
Andrea Parodi <andrea@parro.it>
2016-06-24 11:04:21 +0200

    restored other os & 64bit

--- scripts/start-linux-32.sh
+++ scripts/start-linux-32.sh
@@ -2,8 +2,6 @@
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   docker run -e "TRAVIS_OS_NAME=$TRAVIS_OS_NAME" -e "TRAVIS_TAG=$TRAVIS_TAG" -i -v "${PWD}:/libui-32" 32bit/ubuntu:14.04 /bin/bash -c "linux32 --32bit i386 /libui-32/scripts/run-32.sh"
-
-  ls -l ./artifacts
 else
   echo faked > ./artifacts/examples-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
   echo faked > ./artifacts/libui-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz

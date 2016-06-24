cb8547a2370811b050224ff3ba82762b09eee89e
Andrea Parodi <andrea@parro.it>
2016-06-24 10:08:39 +0200

    changed script path, removed gtk full

--- scripts/run-32.sh
+++ scripts/run-32.sh
@@ -3,5 +3,6 @@
 sudo apt-get update;
 sudo apt-get install libc6-dev-i386 -y;
 sudo apt-get install libgtk-3-dev -y;
-sudo apt-get install gtk+-3.0 -y;
-./scripts/build-32.sh
+# sudo apt-get install gtk+-3.0 -y;
+cd /libui-32;
+./scripts/build-32.sh;

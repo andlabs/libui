bf0f1b0a6443d5b4d4342015e1a727f5dea7b991
Andrea Parodi <andrea@parro.it>
2016-06-24 10:50:50 +0200

    more deps

--- scripts/run-32.sh
+++ scripts/run-32.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 sudo apt-get dist-upgrade -y;
+sudo apt-get update -y;
+
+sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:george-edison55/cmake-3.x -y;
 sudo apt-get update -y;
-sudo apt-get install build-essential git -y;
+
+sudo apt-get install build-essential -y;
 sudo apt-get install cmake -y;
 sudo apt-get install pkg-config -y;
 sudo apt-get install libgtk-3-dev -y;

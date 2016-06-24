e32c77249bf0a41044676a58a52ce022517c9c78
Andrea Parodi <andrea@parro.it>
2016-06-24 09:55:55 +0200

    add ubuntu version

--- scripts/start-linux-32.sh
+++ scripts/start-linux-32.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  docker run -e "TRAVIS_OS_NAME=$TRAVIS_OS_NAME" -e "TRAVIS_TAG=$TRAVIS_TAG" -i -v "${PWD}:/libui-32" 32bit/ubuntu /bin/bash -c "linux32 --32bit i386 /libui-32/scripts/run-32.sh"
-  
-  ls -l ./artifacts	
+  docker run -e "TRAVIS_OS_NAME=$TRAVIS_OS_NAME" -e "TRAVIS_TAG=$TRAVIS_TAG" -i -v "${PWD}:/libui-32" 32bit/ubuntu:14.04 /bin/bash -c "linux32 --32bit i386 /libui-32/scripts/run-32.sh"
+
+  ls -l ./artifacts
 else
   echo faked > ./artifacts/examples-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz
   echo faked > ./artifacts/libui-static-$TRAVIS_OS_NAME-ia32-$TRAVIS_TAG.tar.gz

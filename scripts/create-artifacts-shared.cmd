2a18607ced4334d3be894db80dcd60b919e2884f
Andrea Parodi <andrea@parro.it>
2016-06-24 10:04:34 +0200

    del quietly:gp

--- scripts/create-artifacts-shared.cmd
+++ scripts/create-artifacts-shared.cmd
@@ -5,7 +5,7 @@ copy C:\projects\libui\build\*.h C:\projects\libui\tmp
 7z a -ttar libui-shared-windows-x64-%APPVEYOR_REPO_TAG_NAME%.tar C:\projects\libui\tmp\*.*
 7z a -tgzip libui-shared-windows-x64-%APPVEYOR_REPO_TAG_NAME%.tar.gz libui-shared-windows-x64-%APPVEYOR_REPO_TAG_NAME%.tar
 
-del C:\projects\libui\tmp\*.*
+del C:\projects\libui\tmp\*.* /q
 copy C:\projects\libui\build\out\Release\*.* C:\projects\libui\tmp
 
 7z a -ttar examples-shared-windows-x64-%APPVEYOR_REPO_TAG_NAME%.tar C:\projects\libui\tmp\*.*

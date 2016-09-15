f3d275984b116f79fbb8568e72d1707ef12230c5
Steven Clukey <saclukey@live.com>
2016-09-14 23:44:53 -0400

    Implement getting and setting uiImage size in Gtk

--- unix/image.c
+++ unix/image.c
@@ -8,6 +8,28 @@ struct uiImage {
 
 uiUnixControlAllDefaults(uiImage)
 
+void uiImageSetSize(uiImage *i, unsigned int width, unsigned int height)
+{
+	GdkPixbuf *pixbuf;
+
+	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(i->widget));
+	pixbuf = gdk_pixbuf_scale_simple(pixbuf,
+		width,
+		height,
+		GDK_INTERP_BILINEAR);
+	gtk_image_set_from_pixbuf(GTK_IMAGE(i->widget), pixbuf);
+	g_object_unref(pixbuf);
+}
+
+void uiImageGetSize(uiImage *i, unsigned int *width, unsigned int *height)
+{
+	GdkPixbuf *pixbuf;
+
+	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(i->widget));
+	*width = gdk_pixbuf_get_width(pixbuf);
+	*height = gdk_pixbuf_get_height(pixbuf);
+}
+
 uiImage *uiNewImage(const char *filename)
 {
 	uiImage *img;

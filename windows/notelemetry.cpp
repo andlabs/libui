025f5756cd1693463d5ad78aba0a6136b1a0a636
Thomas Corwin <tacorwin@outlook.com>
2018-01-27 14:27:55 -0500

    Fixed a typo and added a couple comments.

--- windows/notelemetry.cpp
+++ windows/notelemetry.cpp
@@ -1,4 +1,4 @@
-// This file desables MSVC CRT from calling telemetry functions.
+// This file disables MSVC CRT from calling telemetry functions.
 
 // See: https://www.reddit.com/r/cpp/comments/4ibauu/visual_studio_adding_telemetry_function_calls_to/d2wrf57/
 // See also: C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.12.25827\crt\src\linkopts\notelemetry.cpp
@@ -9,8 +9,10 @@ extern "C"
 {
     void __cdecl __vcrt_initialize_telemetry_provider() { }
     void __cdecl __vcrt_uninitialize_telemetry_provider() { }
+    // These were the two on the reddit posts, which I believe are for VS 2015.
     void __cdecl __telemetry_main_invoke_trigger() { }
     void __cdecl __telemetry_main_return_trigger() { }
+    // These two funcions were in the 'notelemetry.cpp' source included with VS 2017, listed in above comment.
     void __cdecl __telemetry_main_invoke_trigger(HINSTANCE) { }
     void __cdecl __telemetry_main_return_trigger(HINSTANCE) { }
 }

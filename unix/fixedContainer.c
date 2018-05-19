5d4f9f7b7364ff38ddfb698b534fc8d937338d1e
kusti8 <kusti8@gmail.com>
2018-05-19 12:57:19 -0400

    Fix syle on container

--- unix/fixedContainer.c
+++ unix/fixedContainer.c
@@ -32,22 +32,22 @@ G_DEFINE_TYPE(fixedContainer, fixedContainer, GTK_TYPE_CONTAINER)
 
 static fixedContainerChild* get_child (fixedContainer  *fixed, GtkWidget *widget)
 {
-  GList *children;
+    GList *children;
 
-  for (children = fixed->children; children; children = children->next)
-    {
-      fixedContainerChild *child;
+    for (children = fixed->children; children; children = children->next) {
+        fixedContainerChild *child;
 
-      child = children->data;
+        child = children->data;
 
-      if (child->widget == widget)
-        return child;
+        if (child->widget == widget)
+            return child;
     }
 
-  return NULL;
+    return NULL;
 }
 
-void fixedContainer_put(fixedContainer *fixed, GtkWidget *widget, gint x, gint y) {
+void fixedContainer_put(fixedContainer *fixed, GtkWidget *widget, gint x, gint y)
+{
     g_return_if_fail (IS_FIXEDCONTAINER (fixed));
     g_return_if_fail (GTK_IS_WIDGET (widget));
 
@@ -61,73 +61,77 @@ void fixedContainer_put(fixedContainer *fixed, GtkWidget *widget, gint x, gint y
     fixed->children = g_list_append(fixed->children, child_info);
 }
 
-void fixedContainer_move(fixedContainer *fixed, GtkWidget *widget, gint x, gint y) {
+void fixedContainer_move(fixedContainer *fixed, GtkWidget *widget, gint x, gint y)
+{
     fixedContainerChild *child = get_child(fixed, widget);
     g_return_if_fail (IS_FIXEDCONTAINER (fixed));
-  g_return_if_fail (gtk_widget_get_parent (child->widget) == GTK_WIDGET (fixed));
+    g_return_if_fail (gtk_widget_get_parent (child->widget) == GTK_WIDGET (fixed));
     
     child->x = x;
     child->y = y;
 
-  if (gtk_widget_get_visible (child->widget) && gtk_widget_get_visible (GTK_WIDGET (fixed)))
-    gtk_widget_queue_resize (GTK_WIDGET (fixed));
+    if (gtk_widget_get_visible (child->widget) && gtk_widget_get_visible (GTK_WIDGET (fixed)))
+        gtk_widget_queue_resize (GTK_WIDGET (fixed));
 }
 
-static void fixedContainer_init(fixedContainer *c) {
+static void fixedContainer_init(fixedContainer *c)
+{
     c->children = NULL;
     gtk_widget_set_has_window(GTK_WIDGET(c), FALSE);
 }
 
-static void fixedContainer_dispose(GObject *obj) {
+static void fixedContainer_dispose(GObject *obj)
+{
     g_list_free(FIXEDCONTAINER(obj)->children);
     G_OBJECT_CLASS(fixedContainer_parent_class)->dispose(obj);
 }
 
-static void fixedContainer_finalize(GObject *obj) {
+static void fixedContainer_finalize(GObject *obj)
+{
     G_OBJECT_CLASS(fixedContainer_parent_class)->finalize(obj);
 }
 
-static void fixedContainer_add(GtkContainer *container, GtkWidget *widget) {
+static void fixedContainer_add(GtkContainer *container, GtkWidget *widget)
+{
     fixedContainer_put(FIXEDCONTAINER(container), widget, 0, 0);
 }
 
-static void fixedContainer_remove(GtkContainer *container, GtkWidget *widget) {
-  fixedContainer *fixed = FIXEDCONTAINER (container);
-  fixedContainerChild *child;
-  GtkWidget *widget_container = GTK_WIDGET (container);
-  GList *children;
+static void fixedContainer_remove(GtkContainer *container, GtkWidget *widget)
+{
+    fixedContainer *fixed = FIXEDCONTAINER (container);
+    fixedContainerChild *child;
+    GtkWidget *widget_container = GTK_WIDGET (container);
+    GList *children;
 
-  for (children = fixed->children; children; children = children->next)
-    {
-      child = children->data;
+    for (children = fixed->children; children; children = children->next) {
+        child = children->data;
 
-      if (child->widget == widget)
-        {
-          gboolean was_visible = gtk_widget_get_visible (widget);
+        if (child->widget == widget) {
+            gboolean was_visible = gtk_widget_get_visible (widget);
 
-          gtk_widget_unparent (widget);
+            gtk_widget_unparent (widget);
 
-          fixed->children = g_list_remove_link (fixed->children, children);
-          g_list_free (children);
-          g_free (child);
+            fixed->children = g_list_remove_link (fixed->children, children);
+            g_list_free (children);
+            g_free (child);
 
-          if (was_visible && gtk_widget_get_visible (widget_container))
+            if (was_visible && gtk_widget_get_visible (widget_container))
             gtk_widget_queue_resize (widget_container);
 
-          break;
+            break;
         }
     }
 }
 
-static void fixedContainer_size_allocate(GtkWidget *widget, GtkAllocation *allocation) {
+static void fixedContainer_size_allocate(GtkWidget *widget, GtkAllocation *allocation)
+{
     fixedContainer *fixed = FIXEDCONTAINER (widget);
     fixedContainerChild *child;
     GtkAllocation child_allocation;
     GtkRequisition child_requisition;
     GList *children;
 
-    for (children = fixed->children; children; children = children->next)
-    {
+    for (children = fixed->children; children; children = children->next) {
         child = children->data;
 
         if (!gtk_widget_get_visible (child->widget))
@@ -143,22 +147,23 @@ static void fixedContainer_size_allocate(GtkWidget *widget, GtkAllocation *alloc
     }
 }
 
-static void fixedContainer_forall(GtkContainer *container, gboolean includeInternals, GtkCallback callback, gpointer callback_data) {
-      fixedContainer *fixed = FIXEDCONTAINER (container);
-  fixedContainerChild *child;
-  GList *children;
+static void fixedContainer_forall(GtkContainer *container, gboolean includeInternals, GtkCallback callback, gpointer callback_data)
+{
+    fixedContainer *fixed = FIXEDCONTAINER (container);
+    fixedContainerChild *child;
+    GList *children;
 
-  children = fixed->children;
-  while (children)
-    {
-      child = children->data;
-      children = children->next;
+    children = fixed->children;
+    while (children) {
+        child = children->data;
+        children = children->next;
 
-      (* callback) (child->widget, callback_data);
+        (* callback) (child->widget, callback_data);
     }
 }
 
-static void fixedContainer_class_init(fixedContainerClass *class) {
+static void fixedContainer_class_init(fixedContainerClass *class)
+{
     G_OBJECT_CLASS(class)->dispose = fixedContainer_dispose;
 	G_OBJECT_CLASS(class)->finalize = fixedContainer_finalize;
 	GTK_WIDGET_CLASS(class)->size_allocate = fixedContainer_size_allocate;
@@ -167,6 +172,7 @@ static void fixedContainer_class_init(fixedContainerClass *class) {
 	GTK_CONTAINER_CLASS(class)->forall = fixedContainer_forall;
 }
 
-GtkWidget* fixedContainer_new(void) {
+GtkWidget* fixedContainer_new(void)
+{
     return g_object_new(FIXEDCONTAINER_TYPE, NULL);
 }

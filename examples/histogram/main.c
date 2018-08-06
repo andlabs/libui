5b4816b7b6448d0be83e405881e540b7e6c14a24
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2018-08-06 23:25:45 +0200

    Restore histogram example

--- examples/histogram/main.c
+++ examples/histogram/main.c
@@ -185,7 +185,7 @@ static int inPoint(double x, double y, double xtest, double ytest)
 		(y <= ytest + pointRadius);
 }
 
-static void handlerMouseEvent(uiAreaEventHandler *ah, uiControl *c, uiAreaMouseEvent *e)
+static void handlerMouseEvent(uiAreaHandler *a, uiArea *area, uiAreaMouseEvent *e)
 {
 	double graphWidth, graphHeight;
 	double xs[10], ys[10];
@@ -205,17 +205,17 @@ static void handlerMouseEvent(uiAreaEventHandler *ah, uiControl *c, uiAreaMouseE
 	uiAreaQueueRedrawAll(histogram);
 }
 
-static void handlerMouseCrossed(uiAreaEventHandler *ah, uiControl *c, int left)
+static void handlerMouseCrossed(uiAreaHandler *ah, uiArea *a, int left)
 {
 	// do nothing
 }
 
-static void handlerDragBroken(uiAreaEventHandler *ah, uiControl *c)
+static void handlerDragBroken(uiAreaHandler *ah, uiArea *a)
 {
 	// do nothing
 }
 
-static int handlerKeyEvent(uiAreaEventHandler *ah, uiControl *c, uiAreaKeyEvent *e)
+static int handlerKeyEvent(uiAreaHandler *ah, uiArea *a, uiAreaKeyEvent *e)
 {
 	// reject all keys
 	return 0;
@@ -253,10 +253,10 @@ int main(void)
 	uiDrawBrush brush;
 
 	handler.Draw = handlerDraw;
-	handler.eh.MouseEvent = handlerMouseEvent;
-	handler.eh.MouseCrossed = handlerMouseCrossed;
-	handler.eh.DragBroken = handlerDragBroken;
-	handler.eh.KeyEvent = handlerKeyEvent;
+	handler.MouseEvent = handlerMouseEvent;
+	handler.MouseCrossed = handlerMouseCrossed;
+	handler.DragBroken = handlerDragBroken;
+	handler.KeyEvent = handlerKeyEvent;
 
 	memset(&o, 0, sizeof (uiInitOptions));
 	err = uiInit(&o);

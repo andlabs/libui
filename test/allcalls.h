671c2031bfe0207ac4c284b9f5e6b391e6d27ffd
Pietro Gagliardi <pietro10@mac.com>
2019-06-02 02:07:33 -0400

    TODO in allcalls.h now resolved.

--- test/allcalls.h
+++ test/allcalls.h
@@ -1,7 +1,6 @@
 // 28 may 2019
 
 // This file should NOT have include guards as it is intended to be included more than once; see noinitwrongthread.c for details.
-// TODO pass in file and line properly
 
 allcallsCase(uiMain, /* no arguments */)
 allcallsCase(uiQuit, /* no arguments */)

822572d395e88c1a2ff75f4a1408692d79d7acc1
Pietro Gagliardi <pietro10@mac.com>
2019-06-08 19:45:09 -0400

    Stubbed out the OS-specific files on macOS so I can actually start writing the uiControl tests.

---
+++ darwin/control.m
@@ -0,0 +1,14 @@
+// 8 june 2019
+#import "uipriv_darwin.h"
+
+bool uiprivOSVtableValid(uiControlOSVtable *osVtable, const char *func)
+{
+	// TODO
+	return true;
+}
+
+uiControlOSVtable *uiprivCloneOSVtable(uiControlOSVtable *osVtable)
+{
+	// TODO
+	return NULL;
+}

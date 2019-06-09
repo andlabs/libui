a2b9145931709f7f1e3c780e01f8e372ee636b1c
Pietro Gagliardi <pietro10@mac.com>
2019-06-09 13:39:14 -0400

    Did the same rewording to most of the other programmer errors. Only the first two are left, because I can't think of a good rewording...

--- common/programmererrors.h
+++ common/programmererrors.h
@@ -11,47 +11,47 @@
 		func)
 
 #define uiprivProgrammerErrorWrongStructSize(badSize, structName, func) \
-	uiprivProgrammerError("wrong size %" uiprivSizetPrintf " for %s in %s()", \
-		badSize, structName, func)
+	uiprivProgrammerError("%s(): wrong size %" uiprivSizetPrintf " for %s", \
+		func, badSize, structName)
 
 #define uiprivProgrammerErrorIndexOutOfRange(badIndex, func) \
-	uiprivProgrammerError("index %d out of range in %s()", \
-		badIndex, func)
+	uiprivProgrammerError("%s(): index %d out of range", \
+		func, badIndex)
 
 #define uiprivProgrammerErrorNullPointer(paramDesc, func) \
-	uiprivProgrammerError("invalid null pointer for %s passed into %s()", \
-		paramDesc, func)
+	uiprivProgrammerError("%s(): invalid null pointer for %s", \
+		func, paramDesc)
 
 // }
 
 // events {
 
 #define uiprivProgrammerErrorEventHandlerNotFound(badID, func) \
-	uiprivProgrammerError("event handler %d not found in %s()", \
-		badID, func)
+	uiprivProgrammerError("%s(): event handler %d not found", \
+		func, badID)
 
 #define uiprivProgrammerErrorBadSenderForEvent(senderDesc, eventDesc, func) \
-	uiprivProgrammerError("attempt to use a %s sender with a %s event in %s()", \
-		senderDesc, eventDesc, func)
+	uiprivProgrammerError("%s(): can't use a %s sender with a %s event", \
+		func, senderDesc, eventDesc)
 
 #define uiprivProgrammerErrorChangingEventDuringFire(func) \
-	uiprivProgrammerError("attempt to change a uiEvent with %s() while it is firing", \
+	uiprivProgrammerError("%s(): can't change a uiEvent while it is firing", \
 		func)
 
 #define uiprivProgrammerErrorRecursiveEventFire(func) \
-	uiprivProgrammerError("attempt to fire a uiEvent while it is already being fired in %s()", \
+	uiprivProgrammerError("%s(): can't recursively fire a uiEvent", \
 		func)
 
 #define uiprivProgrammerErrorFreeingInternalEvent(func) \
-	uiprivProgrammerError("attempt to free a libui-provided uiEvent", \
+	uiprivProgrammerError("%s(): can't free a libui-provided event", \
 		func)
 
 #define uiprivProgrammerErrorFreeingEventInUse(func) \
-	uiprivProgrammerError("attempt to free a uiEvent that still has handlers registered", \
+	uiprivProgrammerError("%s(): can't free event that still has handlers registered", \
 		func)
 
 #define uiprivProgrammerErrorInvalidatingGlobalEvent(func) \
-	uiprivProgrammerError("attempt to call uiEventInvalidateSender() on a global uiEvent", \
+	uiprivProgrammerError("%s(): can't invalidate a global event", \
 		func)
 
 // }

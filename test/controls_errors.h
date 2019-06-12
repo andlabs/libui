47a8ccaf9841b235793868a2e0fb12b58ba59149
Pietro Gagliardi <pietro10@mac.com>
2019-06-12 11:03:21 -0400

    And filled in the error messages in controls_errors.h.

--- test/controls_errors.h
+++ test/controls_errors.h
@@ -1,12 +1,12 @@
 // 11 june 2019
 
 checkErrorCase(uiRegisterControlType(NULL, p->vtablePlaceholder, p->osVtablePlaceholder, p->implDataSizePlaceholder),
-	"TODO")
+	"uiRegisterControlType(): invalid null pointer for uiControlOSVtable")
 checkErrorCase(uiRegisterControlType(p->namePlaceholder, NULL, p->osVtablePlaceholder, p->implDataSizePlaceholder),
-	"TODO")
+	"uiRegisterControlType(): invalid null pointer for uiControlVtable")
 checkErrorCase(uiRegisterControlType(p->namePlaceholder, p->vtableBadSize, p->osVtablePlaceholder, p->implDataSizePlaceholder),
-	"TODO")
+	"uiRegisterControlType(): wrong size 1 for uiControlVtable")
 // TODO individual methods
 checkErrorCase(uiRegisterControlType(p->namePlaceholder, p->vtablePlaceholder, NULL, p->implDataSizePlaceholder),
-	"TODO")
+	"uiRegisterControlType(): invalid null pointer for uiControlOSVtable")
 // OS vtable sizes are tested per-OS

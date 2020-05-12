de5205d594f2e393e2678094ce99ec051628e8ba
Pietro Gagliardi <pietro10@mac.com>
2020-05-11 22:37:02 -0400

    And with Unix.

---
+++ test/controls_unix.c
@@ -0,0 +1,25 @@
+// 10 june 2019
+#include "test_unix.h"
+
+static const uiControlOSVtable osVtable = {
+	.Size = sizeof (uiControlOSVtable),
+};
+
+const uiControlOSVtable *testOSVtable(void)
+{
+	return &osVtable;
+}
+
+Test(WrongControlOSVtableSizeIsProgrammerError)
+{
+	uiControlVtable vtable;
+	uiControlOSVtable osvt;
+	void *ctx;
+
+	testControlLoadNopVtable(&vtable);
+	ctx = beginCheckProgrammerError("uiRegisterControlType(): wrong size 1 for uiControlOSVtable");
+	memset(&osvt, 0, sizeof (uiControlOSVtable));
+	osvt.Size = 1;
+	uiRegisterControlType("name", &vtable, &osvt, 0);
+	endCheckProgrammerError(ctx);
+}

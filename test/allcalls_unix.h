d5c06108d69374b0e4558749c8bc793e4a5482c3
Pietro Gagliardi <pietro10@mac.com>
2020-06-02 22:52:56 -0400

    Added the missing noinitwrongthread Unix tests.

---
+++ test/allcalls_unix.h
@@ -0,0 +1,5 @@
+// 28 may 2019
+
+// This file should NOT have include guards as it is intended to be included more than once; see noinitwrongthread_unix.c for details.
+
+allcallsCase(uiUnixControlHandle, NULL)
---
+++ test/noinitwrongthread_unix.c
@@ -0,0 +1,6 @@
+// 28 may 2019
+#include "test_unix.h"
+#include "thread.h"
+
+#define allcallsHeader "allcalls_unix.h"
+#include "noinitwrongthreadimpl.h"
